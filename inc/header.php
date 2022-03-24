9a8feac8f32aed1d0c895dd62151715ccb8362ae
Taemin1234 <86579707+Taemin1234@users.noreply.github.com>
2022-03-24 17:49:29 +0900

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Data Lab</title>
-    <link rel="stylesheet" href="./css/style.css" />
-    <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="/css/footer.css">
 
     <!-- 폰트어썸 cdn -->
     <link
@@ -15,12 +15,12 @@
     />
 
     <!-- jquery -->
-    <script src="./jquery-3.6.0.min.js"></script>
+    <script src="/jquery-3.6.0.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <!-- slick 슬라이더 js, cdn -->
-    <script src="./slick-1.8.1/slick/slick.min.js"></script>
-    <link rel="stylesheet" href="./slick-1.8.1/slick/slick.css" />
+    <script src="/slick-1.8.1/slick/slick.min.js"></script>
+    <link rel="stylesheet" href="/slick-1.8.1/slick/slick.css" />
     <!-- <script
       type="text/javascript"
       src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
@@ -31,5 +31,8 @@
       href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
     /> -->
 
-    <script src="./js/app.js"></script>
+    <!-- chart.js cdn -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+
+    <script src="/js/app.js"></script>
   </head>
