eefcecb018d36d5ad9fd97675c82bb26b0ed2b44
Taemin1234 <86579707+Taemin1234@users.noreply.github.com>
2022-04-04 08:34:32 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -141,7 +141,7 @@ $(function () {
   });
 });
 
-// 인사이트 기간 설정 버튼
+// 인사이트 기간 설정 버튼 클릭 시 UI 변경
 $(function () {
   var idx;
   $(".period-input-btn button").click(function () {
@@ -154,9 +154,6 @@ $(function () {
 });
 
 //인사이트 기본 날짜 설정
-
-// 올해 월이 x월이라면 end-date에서 select option의 월을 x월로 기본설정해두어라
-// 년도도 같이
 
 $(function () {
   var now = new Date();
@@ -187,17 +184,54 @@ $(function () {
       $(".start-date #select-year option").eq(i).prop("selected", true);
     }
   }
+});
 
-  // 올해 날짜가 선택되었으면 날짜는 저번달까지만 보이게 해라
-  // start date보다 end date의 년도가 같거나 커야한다.
+// 올해 날짜가 선택되었으면 날짜는 저번달까지만 보이게 해라
+// start date보다 end date의 년도가 같거나 커야한다.
+$(function () {
+  var startYear = parseInt($(".start-date #select-year option:selected").val());
+
+  $("select[name=start-year], select[name=start-month]").change(function () {
+    console.log(startYear);
+  });
+  // if() {
+
+  // }
+
+  // for(var i = startYear; i<".end-date #select-year option".length; i++) {
+  //   console.log(parseInt((".start-date #select-year option:selected").eq(i).val()))
+  // }
+
+  //$("select[name=start-year]").change(function () {
+  //  $(".start-year").text($(this).val()); value값 출력
+  //  $("select[name=start-year] option:selected").text(); text값 출력
+  // });
+
+  // $("select[name=start-month]").change(function () {
+  //   $(".start-month").text($(this).val());
+  // });
+
+  // $("select[name=end-year]").change(function () {
+  //   $(".end-year").text($(this).val());
+  // });
+
+  // $("select[name=end-month]").change(function () {
+  //   $(".end-month").text($(this).val());
+  // });
 });
 
 $(function () {
-  var now = new Date();
+  // 현재 연도와 월
+  var year = moment().format("yyyy");
+  var month = moment().format("MM");
 
-  var year = now.getFullYear();
-  var month = now.getMonth() + 1;
+  // 3개월전, 1년전 연도와 월
+  var year3m = moment().add(-3, "M").format("yyyy");
+  var month3m = moment().add(-3, "M").format("MM");
+  var year1y = moment().add(-1, "y").format("yyyy");
+  var month1y = moment().add(-1, "y").format("MM");
 
+  //직전 3개월 버튼
   $(".period-3month").on("click", function () {
     // 현재 날짜에서 3개월을 빼고 만약 3월 이하라면 10더하기
     for (var i = 0; i < 12; i++) {
@@ -207,22 +241,93 @@ $(function () {
           .prop("selected", true);
       }
 
-      if ($(".end-date #select-month option").eq(i).val() < 3) {
-        $(".start-date #select-month option")
-          .eq(i + 10)
+      //1월과 2월은 작년 11월과 12월로 설정
+      if ($(".end-date #select-month option").eq(i).val() == month3m) {
+        $(".start-date #select-month option").eq(i).prop("selected", true);
+      }
+    }
+
+    //연도 설정
+    for (var i = 0; i < ".end-date #select-year option".length; i++) {
+      if (
+        parseInt($(".start-date #select-year option").eq(i).val()) == year3m
+      ) {
+        $(".start-date #select-year option").eq(i).prop("selected", true);
+      }
+    }
+
+    $(
+      "select[name=end-year], select[name=end-month], select[name=start-month], select[name=start-year]"
+    ).attr("disabled", true);
+  });
+
+  //직전 1개월 버튼 클릭
+  $(".period-select").on("click", function () {
+    for (var i = 0; i < 12; i++) {
+      if ($(".end-date #select-month option").eq(i).val() == month) {
+        $(".end-date #select-month option")
+          .eq(i - 1)
           .prop("selected", true);
-      } else {
+      }
+
+      if ($(".start-date #select-month option").eq(i).val() == month) {
         $(".start-date #select-month option")
-          .eq(i - 2)
+          .eq(i - 1)
           .prop("selected", true);
       }
     }
 
+    //연도설정
     for (var i = 0; i < ".end-date #select-year option".length; i++) {
       if ($(".end-date #select-year option").eq(i).val() == year) {
         $(".end-date #select-year option").eq(i).prop("selected", true);
       }
+
+      if ($(".start-date #select-year option").eq(i).val() == year) {
+        $(".start-date #select-year option").eq(i).prop("selected", true);
+      }
     }
+
+    $(
+      "select[name=end-year], select[name=end-month], select[name=start-month], select[name=start-year]"
+    ).attr("disabled", true);
+  });
+
+  //직전 1년 버튼을 누르면 1년의 데이터를 보여줌
+  $(".period-1year").on("click", function () {
+    //월 설정
+    for (var i = 0; i < 12; i++) {
+      if ($(".end-date #select-month option").eq(i).val() == month) {
+        $(".end-date #select-month option")
+          .eq(i - 1)
+          .prop("selected", true);
+      }
+
+      if ($(".end-date #select-month option").eq(i).val() == month1y) {
+        $(".start-date #select-month option").eq(i).prop("selected", true);
+      }
+    }
+
+    //년도 설정
+
+    for (var i = 0; i < ".end-date #select-year option".length; i++) {
+      if (
+        parseInt($(".start-date #select-year option").eq(i).val()) == year1y
+      ) {
+        $(".start-date #select-year option").eq(i).prop("selected", true);
+      }
+    }
+
+    $(
+      "select[name=end-year], select[name=end-month], select[name=start-month], select[name=start-year]"
+    ).attr("disabled", true);
+  });
+
+  //직접입력
+  $(".period-custom").on("click", function () {
+    $(
+      "select[name=end-year], select[name=end-month], select[name=start-month], select[name=start-year]"
+    ).removeAttr("disabled");
   });
 });
 
