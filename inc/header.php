31e1228937ba6d49e260b6cb569442b308bb212c
Taemin1234 <86579707+Taemin1234@users.noreply.github.com>
2022-04-04 09:54:39 +0900

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -34,4 +34,14 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 
     <script src="/js/app.js"></script>
+
+    <!-- monment.js -->
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"
+      integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    ></script>
+
+
   </head>
